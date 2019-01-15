deebb80d053b36b0a610d773a2a7536080a4d5cf
Hakan Gedek <hgedek@gmail.com>
2019-01-15 13:38:37 +0300

    first commit

---
+++ gstsample/jni/native_lib.c
@@ -0,0 +1,377 @@
+#include <string.h>
+#include <jni.h>
+#include <android/log.h>
+#include <gst/gst.h>
+#include <pthread.h>
+#include <gst/rtsp-server/rtsp-server.h>
+
+GST_DEBUG_CATEGORY_STATIC (debug_category);
+#define DEFAULT_RTSP_PORT "5000"
+
+/*
+ * These macros provide a way to store the native pointer to CustomData, which might be 32 or 64 bits, into
+ * a jlong, which is always 64 bits, without warnings.
+ */
+#if GLIB_SIZEOF_VOID_P == 8
+# define GET_CUSTOM_DATA(env, thiz, fieldID) (CustomData *)(*env)->GetLongField (env, thiz, fieldID)
+# define SET_CUSTOM_DATA(env, thiz, fieldID, data) (*env)->SetLongField (env, thiz, fieldID, (jlong)data)
+#else
+# define GET_CUSTOM_DATA(env, thiz, fieldID) (CustomData *)(jint)(*env)->GetLongField (env, thiz, fieldID)
+# define SET_CUSTOM_DATA(env, thiz, fieldID, data) (*env)->SetLongField (env, thiz, fieldID, (jlong)(jint)data)
+#endif
+
+/* Structure to contain all our information, so we can pass it to callbacks */
+typedef struct _CustomData {
+  jobject app;           /* Application instance, used to call its methods. A global reference is kept. */
+  GstElement *pipeline;  /* The running pipeline */
+  GMainContext *context; /* GLib context used to run the main loop */
+  GMainLoop *main_loop;  /* GLib main loop */
+  gboolean initialized;  /* To avoid informing the UI multiple times about the initialization */
+} CustomData;
+
+/* These global variables cache values which are not changing during execution */
+static pthread_t gst_app_thread;
+static pthread_key_t current_jni_env;
+static JavaVM *java_vm;
+static jfieldID custom_data_field_id;
+static jmethodID set_message_method_id;
+static jmethodID on_gstreamer_initialized_method_id;
+
+/*
+ * Private methods
+ */
+
+/* Register this thread with the VM */
+static JNIEnv *attach_current_thread (void) {
+  JNIEnv *env;
+  JavaVMAttachArgs args;
+
+  GST_DEBUG ("Attaching thread %p", g_thread_self ());
+  args.version = JNI_VERSION_1_4;
+  args.name = NULL;
+  args.group = NULL;
+
+  if ((*java_vm)->AttachCurrentThread (java_vm, &env, &args) < 0) {
+    GST_ERROR ("Failed to attach current thread");
+    return NULL;
+  }
+
+  return env;
+}
+
+/* Unregister this thread from the VM */
+static void detach_current_thread (void *env) {
+  GST_DEBUG ("Detaching thread %p", g_thread_self ());
+  (*java_vm)->DetachCurrentThread (java_vm);
+}
+
+/* Retrieve the JNI environment for this thread */
+static JNIEnv *get_jni_env (void) {
+  JNIEnv *env;
+
+  if ((env = pthread_getspecific (current_jni_env)) == NULL) {
+    env = attach_current_thread ();
+    pthread_setspecific (current_jni_env, env);
+  }
+
+  return env;
+}
+
+/* Change the content of the UI's TextView */
+static void set_ui_message (const gchar *message, CustomData *data) {
+  JNIEnv *env = get_jni_env ();
+  GST_DEBUG ("Setting message to: %s", message);
+  jstring jmessage = (*env)->NewStringUTF(env, message);
+  (*env)->CallVoidMethod (env, data->app, set_message_method_id, jmessage);
+  if ((*env)->ExceptionCheck (env)) {
+    GST_ERROR ("Failed to call Java method");
+    (*env)->ExceptionClear (env);
+  }
+  (*env)->DeleteLocalRef (env, jmessage);
+}
+
+/* Retrieve errors from the bus and show them on the UI */
+static void error_cb (GstBus *bus, GstMessage *msg, CustomData *data) {
+  GError *err;
+  gchar *debug_info;
+  gchar *message_string;
+
+  gst_message_parse_error (msg, &err, &debug_info);
+  message_string = g_strdup_printf ("Error received from element %s: %s", GST_OBJECT_NAME (msg->src), err->message);
+  g_clear_error (&err);
+  g_free (debug_info);
+  set_ui_message (message_string, data);
+  g_free (message_string);
+  gst_element_set_state (data->pipeline, GST_STATE_NULL);
+}
+
+/* Notify UI about pipeline state changes */
+static void state_changed_cb (GstBus *bus, GstMessage *msg, CustomData *data) {
+  GstState old_state, new_state, pending_state;
+  gst_message_parse_state_changed (msg, &old_state, &new_state, &pending_state);
+  /* Only pay attention to messages coming from the pipeline, not its children */
+  if (GST_MESSAGE_SRC (msg) == GST_OBJECT (data->pipeline)) {
+    gchar *message = g_strdup_printf("State changed to %s", gst_element_state_get_name(new_state));
+    set_ui_message(message, data);
+    g_free (message);
+  }
+}
+
+/* Check if all conditions are met to report GStreamer as initialized.
+ * These conditions will change depending on the application */
+static void check_initialization_complete (CustomData *data) {
+  JNIEnv *env = get_jni_env ();
+  if (!data->initialized && data->main_loop) {
+    GST_DEBUG ("Initialization complete, notifying application. main_loop:%p", data->main_loop);
+    (*env)->CallVoidMethod (env, data->app, on_gstreamer_initialized_method_id);
+    if ((*env)->ExceptionCheck (env)) {
+      GST_ERROR ("Failed to call Java method");
+      (*env)->ExceptionClear (env);
+    }
+    data->initialized = TRUE;
+  }
+}
+
+typedef struct
+{
+    gboolean white;
+    GstClockTime timestamp;
+} MyContext;
+
+/* called when we need to give data to appsrc */
+static void
+need_data (GstElement * appsrc, guint unused, MyContext * ctx)
+{
+  GstBuffer *buffer;
+  guint size;
+  GstFlowReturn ret;
+
+  size = 385 * 288 * 2;
+
+  buffer = gst_buffer_new_allocate (NULL, size, NULL);
+
+  /* this makes the image black/white */
+  gst_buffer_memset (buffer, 0, ctx->white ? 0xff : 0x0, size);
+
+  ctx->white = !ctx->white;
+
+  /* increment the timestamp every 1/2 second */
+  GST_BUFFER_PTS (buffer) = ctx->timestamp;
+  GST_BUFFER_DURATION (buffer) = gst_util_uint64_scale_int (1, GST_SECOND, 2);
+  ctx->timestamp += GST_BUFFER_DURATION (buffer);
+
+  g_signal_emit_by_name (appsrc, "push-buffer", buffer, &ret);
+}
+
+/* called when a new media pipeline is constructed. We can query the
+ * pipeline and configure our appsrc */
+static void
+media_configure (GstRTSPMediaFactory * factory, GstRTSPMedia * media,
+                 gpointer user_data)
+{
+  GstElement *element, *appsrc;
+  MyContext *ctx;
+
+  /* get the element used for providing the streams of the media */
+  element = gst_rtsp_media_get_element (media);
+
+  /* get our appsrc, we named it 'mysrc' with the name property */
+  appsrc = gst_bin_get_by_name_recurse_up (GST_BIN (element), "mysrc");
+
+  /* this instructs appsrc that we will be dealing with timed buffer */
+  gst_util_set_object_arg (G_OBJECT (appsrc), "format", "time");
+  /* configure the caps of the video */
+  g_object_set (G_OBJECT (appsrc), "caps",
+                gst_caps_new_simple ("video/x-raw",
+                                     "format", G_TYPE_STRING, "RGB16",
+                                     "width", G_TYPE_INT, 384,
+                                     "height", G_TYPE_INT, 288,
+                                     "framerate", GST_TYPE_FRACTION, 0, 1, NULL), NULL);
+
+  ctx = g_new0 (MyContext, 1);
+  ctx->white = FALSE;
+  ctx->timestamp = 0;
+  /* make sure ther datais freed when the media is gone */
+  g_object_set_data_full (G_OBJECT (media), "my-extra-data", ctx,
+                          (GDestroyNotify) g_free);
+
+  /* install the callback that will be called when a buffer is needed */
+  g_signal_connect (appsrc, "need-data", (GCallback) need_data, ctx);
+  gst_object_unref (appsrc);
+  gst_object_unref (element);
+}
+
+static void* app_function2(void *userdata){
+    GMainLoop *loop;
+    GstRTSPServer *server;
+    GstRTSPMountPoints *mounts;
+    GstRTSPMediaFactory *factory;
+
+    loop = g_main_loop_new (NULL, FALSE);
+
+    server = gst_rtsp_server_new ();
+
+    g_object_set (server, "service", DEFAULT_RTSP_PORT, NULL);
+
+    mounts = gst_rtsp_server_get_mount_points (server);
+
+    factory = gst_rtsp_media_factory_new ();
+    gst_rtsp_media_factory_set_launch (factory,
+                                       "( ahcsrc ! videoconvert ! avenc_h263 ! rtph263ppay name=pay0 pt=96 )");
+
+    gst_rtsp_mount_points_add_factory (mounts, "/test", factory);
+
+    g_object_unref (mounts);
+
+    gst_rtsp_server_attach (server, NULL);
+
+    g_print ("stream ready at rtsp://127.0.0.1:8554/test\n");
+    g_main_loop_run (loop);
+
+    return NULL;
+}
+
+/* Main method for the native code. This is executed on its own thread. */
+static void *app_function (void *userdata) {
+
+  JavaVMAttachArgs args;
+  GstBus *bus;
+  CustomData *data = (CustomData *)userdata;
+  GSource *bus_source;
+  GError *error = NULL;
+
+  GST_DEBUG ("Creating pipeline in CustomData at %p", data);
+
+  data->context = g_main_context_new ();
+  g_main_context_push_thread_default(data->context);
+
+/*  data->pipeline = gst_parse_launch("rtpbin name=rtpbin "
+                                    "udpsrc port=5002  caps=\"application/x-rtp,clock-rate=48000,payload=(int)96,media=(string)audio,encoding-name=(string)OPUS\" ! "
+                                    "rtpbin.recv_rtp_sink_0  rtpbin. ! rtpopusdepay ! opusdec ! audioconvert ! audioresample ! openslessink "
+                                    "udpsrc port=5003 ! rtpbin.recv_rtcp_sink_0  rtpbin.send_rtcp_src_0 ! udpsink port=6007 host=192.168.1.12 sync=false async=false",
+                                    &error);*/
+
+    //video streaming
+    data->pipeline = gst_parse_launch("rtpbin name=rtpbin ahcsrc ! videoconvert ! avenc_h263 ! rtph263ppay ! rtpbin.send_rtp_sink_0 rtpbin.send_rtp_src_0 ! "
+                                      "udpsink port=5000 host=192.168.1.12 rtpbin.send_rtcp_src_0 ! udpsink port=5001 port=5000 host=192.168.1.12 sync=false async=false "
+                                      "udpsrc port=5005 ! rtpbin.recv_rtcp_sink_0"
+                                      ,&error);
+  if (error) {
+    gchar *message = g_strdup_printf("Unable to build pipeline: %s", error->message);
+    g_clear_error (&error);
+    set_ui_message(message, data);
+    g_free (message);
+    return NULL;
+  }
+
+  bus = gst_element_get_bus (data->pipeline);
+  bus_source = gst_bus_create_watch (bus);
+  g_source_set_callback (bus_source, (GSourceFunc) gst_bus_async_signal_func, NULL, NULL);
+  g_source_attach (bus_source, data->context);
+  g_source_unref (bus_source);
+  g_signal_connect (G_OBJECT (bus), "message::error", (GCallback)error_cb, data);
+  g_signal_connect (G_OBJECT (bus), "message::state-changed", (GCallback)state_changed_cb, data);
+  gst_object_unref (bus);
+
+  GST_DEBUG ("Entering main loop... (CustomData:%p)", data);
+  data->main_loop = g_main_loop_new (data->context, FALSE);
+  check_initialization_complete (data);
+  g_main_loop_run (data->main_loop);
+  GST_DEBUG ("Exited main loop");
+  g_main_loop_unref (data->main_loop);
+  data->main_loop = NULL;
+
+  g_main_context_pop_thread_default(data->context);
+  g_main_context_unref (data->context);
+  gst_element_set_state (data->pipeline, GST_STATE_NULL);
+  gst_object_unref (data->pipeline);
+
+  return NULL;
+}
+
+/* Instruct the native code to create its internal data structure, pipeline and thread */
+static void gst_native_init (JNIEnv* env, jobject thiz) {
+  CustomData *data = g_new0 (CustomData, 1);
+  SET_CUSTOM_DATA (env, thiz, custom_data_field_id, data);
+  GST_DEBUG_CATEGORY_INIT (debug_category, "gstsample", 0, "Android tutorial 2");
+  gst_debug_set_threshold_for_name("gstsample", GST_LEVEL_DEBUG);
+  GST_DEBUG ("Created CustomData at %p", data);
+  data->app = (*env)->NewGlobalRef (env, thiz);
+  GST_DEBUG ("Created GlobalRef for app object at %p", data->app);
+  pthread_create (&gst_app_thread, NULL, &app_function2, data);
+}
+
+/* Quit the main loop, remove the native thread and free resources */
+static void gst_native_finalize (JNIEnv* env, jobject thiz) {
+  CustomData *data = GET_CUSTOM_DATA (env, thiz, custom_data_field_id);
+  if (!data) return;
+  GST_DEBUG ("Quitting main loop...");
+  g_main_loop_quit (data->main_loop);
+  GST_DEBUG ("Waiting for thread to finish...");
+  pthread_join (gst_app_thread, NULL);
+  GST_DEBUG ("Deleting GlobalRef for app object at %p", data->app);
+  (*env)->DeleteGlobalRef (env, data->app);
+  GST_DEBUG ("Freeing CustomData at %p", data);
+  g_free (data);
+  SET_CUSTOM_DATA (env, thiz, custom_data_field_id, NULL);
+  GST_DEBUG ("Done finalizing");
+}
+
+/* Set pipeline to PLAYING state */
+static void gst_native_play (JNIEnv* env, jobject thiz) {
+  CustomData *data = GET_CUSTOM_DATA (env, thiz, custom_data_field_id);
+  if (!data) return;
+  GST_DEBUG ("Setting state to PLAYING");
+  gst_element_set_state (data->pipeline, GST_STATE_PLAYING);
+}
+
+/* Set pipeline to PAUSED state */
+static void gst_native_pause (JNIEnv* env, jobject thiz) {
+  CustomData *data = GET_CUSTOM_DATA (env, thiz, custom_data_field_id);
+  if (!data) return;
+  GST_DEBUG ("Setting state to PAUSED");
+  gst_element_set_state (data->pipeline, GST_STATE_PAUSED);
+}
+
+/* Static class initializer: retrieve method and field IDs */
+static jboolean gst_native_class_init (JNIEnv* env, jclass klass) {
+  custom_data_field_id = (*env)->GetFieldID (env, klass, "native_custom_data", "J");
+  set_message_method_id = (*env)->GetMethodID (env, klass, "setMessage", "(Ljava/lang/String;)V");
+  on_gstreamer_initialized_method_id = (*env)->GetMethodID (env, klass, "onGStreamerInitialized", "()V");
+
+  if (!custom_data_field_id || !set_message_method_id || !on_gstreamer_initialized_method_id) {
+    /* We emit this message through the Android log instead of the GStreamer log because the later
+     * has not been initialized yet.
+     */
+    __android_log_print (ANDROID_LOG_ERROR, "gstsample", "The calling class does not implement all necessary interface methods");
+    return JNI_FALSE;
+  }
+  return JNI_TRUE;
+}
+
+/* List of implemented native methods */
+static JNINativeMethod native_methods[] = {
+  { "nativeInit", "()V", (void *) gst_native_init},
+  { "nativeFinalize", "()V", (void *) gst_native_finalize},
+  { "nativePlay", "()V", (void *) gst_native_play},
+  { "nativePause", "()V", (void *) gst_native_pause},
+  { "nativeClassInit", "()Z", (void *) gst_native_class_init}
+};
+
+/* Library initializer */
+jint JNI_OnLoad(JavaVM *vm, void *reserved) {
+  JNIEnv *env = NULL;
+
+  java_vm = vm;
+
+  if ((*vm)->GetEnv(vm, (void**) &env, JNI_VERSION_1_4) != JNI_OK) {
+    __android_log_print (ANDROID_LOG_ERROR, "gstsample", "Could not retrieve JNIEnv");
+    return 0;
+  }
+  jclass klass = (*env)->FindClass (env, "org/freedesktop/gstreamer/MainActivity");
+  (*env)->RegisterNatives (env, klass, native_methods, G_N_ELEMENTS(native_methods));
+
+  pthread_key_create (&current_jni_env, detach_current_thread);
+
+  return JNI_VERSION_1_4;
+}
